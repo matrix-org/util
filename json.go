2e2df66af2f5dcf30843b5793134a0f0ce9338c6
Richard van der Hoff <github@rvanderhoff.org.uk>
2017-11-27 12:17:16 +0000

    Add Authorization to CORS headers (#17)
    So that it works with riot-web develop

--- json.go
+++ json.go
@@ -176,7 +176,7 @@ func WithCORSOptions(handler http.HandlerFunc) http.HandlerFunc {
 func SetCORSHeaders(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 }
 
 const alphanumerics = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
